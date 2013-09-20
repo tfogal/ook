e22abae7581cbd3ac8bcf85f9b33038cf0b1bce7
Tom Fogal <tfogal@alumni.unh.edu>
2013-09-20 11:53:14 -0600

    add 'writer' interface (unimplemented right now).

--- io-interface.h
+++ io-interface.h
@@ -23,6 +23,17 @@ typedef void* (opener)(const char* fn, const enum OOKMODE);
 typedef int (reader)(void* fd, const off_t offset, const size_t len,
                      void* buf);
 
+/** The writer interface wraps basic 'write' operations.
+ * It needs a 'descriptor', an offset, and length.  It writes from the given
+ * buffer.
+ * @param fd a descriptor returned from the 'open' interface.
+ * @note ook's writer interface is atomic: partial writes are not possible.  if
+ *       only a portion of the data are successfully written, an error is
+ *       returned (with no way to identify which portion).
+ * @returns 0 on success, an error code (l.t. 0) on error. */
+typedef int (writer)(void* fd, const off_t offset, const size_t len,
+                     const void* buf);
+
 /** The closer interface simply closes something opened by an opener.
  * @returns 0 on success, an error code < 0 on error. */
 typedef int (closer)(void* fd);
@@ -37,6 +48,7 @@ extern reader* stdc_reader;
 struct io {
   opener* open;
   reader* read;
+  writer* write;
   closer* close;
   prealloc* preallocate;
 };
