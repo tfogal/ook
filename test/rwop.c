09783a395fe9317f2dcd3665e5242cc90eb6e8aa
Tom Fogal <tfogal@alumni.unh.edu>
2013-09-24 13:16:17 -0600

    Add a test for multicomponent data.

--- test/rwop.c
+++ test/rwop.c
@@ -3,12 +3,24 @@
 #include <check.h>
 #include "ook.h"
 
+/* older versions of check don't have pointer tests, so we hack it with integer
+ * tests.  but I guess type checking is stricter in newer check; the int tests
+ * don't work for pointers anymore. */
+#if CHECK_MINOR_VERSION == 9 && CHECK_MICRO_VERSION >= 10
+# define tjf_ck_ptr_eq(a,b) ck_assert_ptr_eq(a,b)
+# define tjf_ck_ptr_ne(a,b) ck_assert_ptr_ne(a,b)
+#else
+# define tjf_ck_ptr_eq(a,b) ck_assert_int_eq(a,b)
+# define tjf_ck_ptr_ne(a,b) ck_assert_int_ne(a,b)
+#endif
+
 static struct ookfile* zeroes;
 static const char* simplefile = ".testing";
+static const char* multifile = ".testing-multicomponent";
 static struct ookfile* of;
 
 static void
-print_arr(const uint32_t* d, size_t n)
+print_arru32(const uint32_t* d, size_t n)
 {
   printf("{ ");
   for(size_t i=0; i < n; ++i) {
@@ -18,12 +30,22 @@ print_arr(const uint32_t* d, size_t n)
 }
 
 void
-print_slice(const uint32_t* d, size_t slice, const size_t nx, const size_t ny)
+print_arru16(const uint16_t* d, const size_t n)
+{
+  printf("{ ");
+  for(size_t i=0; i < n; ++i) {
+    printf("%hu ", d[i]);
+  }
+  printf("}\n");
+}
+
+void
+print_slice(const uint32_t* d, size_t slice, const size_t nx,const size_t ny)
 {
   const size_t z = slice;
   for(size_t y=0; y < ny; ++y) {
     const size_t coord = z*ny*nx + y*nx;
-    print_arr(d+coord, nx);
+    print_arru32(d+coord, nx);
   }
 }
 
@@ -33,35 +55,107 @@ value(size_t x, size_t y, size_t z)
   return 25700U + z/16 + (y*(16/2)) + x;
 }
 
-static void
-setup_simple()
+void
+print_all(const uint16_t* d, const size_t bsize[3], const size_t components)
 {
-  FILE* fp = fopen(simplefile, "w+b");
-  ck_assert_int_ne(fp, NULL);
-  uint32_t data[16];
-  for(size_t z=0; z < 16; ++z) {
-    for(size_t y=0; y < 16; ++y) {
-      for(size_t x=0; x < 16; ++x) {
-        data[x] = value(x,y,z);
+  const size_t c=components; /* convenience. */
+  for(size_t z=0; z < bsize[2]; ++z) {
+    printf("+++++++++++++++++++++++++++\n");
+    for(size_t y=0; y < bsize[1]; ++y) {
+      printf("| ");
+      for(size_t x=0; x < bsize[0]*c; ++x) {
+        printf("%5hu ", d[z*bsize[1]*bsize[0]*c + y*bsize[0]*c + x]);
       }
-      fwrite(data, sizeof(uint32_t), 16, fp);
+      printf("|\n");
+    }
+  }
+}
+
+void
+print_function(const size_t bsize[3])
+{
+  for(size_t z=0; z < bsize[2]; ++z) {
+    printf("+++++++++++++++++++++++++++\n");
+    for(size_t y=0; y < bsize[1]; ++y) {
+      printf(": ");
+      for(size_t x=0; x < bsize[0]*2; ++x) {
+        if((x % 2) == 1) {
+					printf("%5hu ", value(x,y,z) % 16);
+        } else {
+					printf("%5hu ", value(x,y,z));
+        }
+      }
+      printf(":\n");
+    }
+  }
+}
+
+static void
+setup_multicomp()
+{
+  const size_t dims[3] = { 4, 4, 4 };
+
+  FILE* fp = fopen(multifile, "wb");
+  tjf_ck_ptr_ne(fp, NULL);
+  uint16_t data[dims[0]*2];
+  for(size_t z=0; z < dims[2]; ++z) {
+    for(size_t y=0; y < dims[1]; ++y) {
+      for(size_t x=0; x < dims[0]*2; x+=2) {
+				data[x+0] = value(x,y,z);
+				data[x+1] = value(x,y,z) % 16;
+      }
+      fwrite(data, sizeof(uint16_t), dims[0]*2, fp);
     }
   }
   fclose(fp);
   ck_assert(ookinit(StdCIO));
 
-  const uint64_t sz[3] = { 16, 16, 16 };
-  const size_t bsize[3] = { 8, 8, 16 };
-  of = ookread(simplefile, sz, bsize, OOK_U32, 1);
-  ck_assert_int_ne(of, NULL);
+  const uint64_t sz[3] = { dims[0], dims[1], dims[2] };
+  const size_t bsize[3] = { dims[0]/2, dims[1],  dims[2]/2 };
+  of = ookread(multifile, sz, bsize, OOK_U16, 2);
+  tjf_ck_ptr_ne(of, NULL);
 }
 
 static void
-teardown_simple()
+teardown_multicomp()
 {
   ck_assert(ookclose(of) == 0);
   of = NULL; /* force memory being unreachable (for leak checking) */
 }
+
+START_TEST(multicomp_read)
+{
+  size_t bsize[3];
+  ookmaxbricksize(of, bsize);
+  /* bricksize is stored correctly? */
+  ck_assert_int_eq(bsize[0], 2U);
+  ck_assert_int_eq(bsize[1], 4U);
+  ck_assert_int_eq(bsize[2], 2U);
+
+  const size_t components = 2;
+  const size_t bytes_brick = sizeof(uint16_t) * components *
+                             bsize[0]*bsize[1]*bsize[2];
+  uint16_t* data = malloc(bytes_brick);
+  memset(data, 0, bytes_brick);
+  ookbrick(of, 0, data);
+
+  for(size_t z=0; z < bsize[2]; ++z) {
+    for(size_t y=0; y < bsize[1]; ++y) {
+      for(size_t x=0; x < bsize[0]*2; x+=2) {
+        ck_assert_int_eq(
+          data[z*bsize[1]*bsize[0]*2 + y*bsize[0]*2 + x + 0],
+          value(x,y,z)
+        );
+        ck_assert_int_eq(
+          data[z*bsize[1]*bsize[0]*2 + y*bsize[0]*2 + x + 1],
+          value(x,y,z) % 16
+        );
+      }
+    }
+  }
+  free(data);
+}
+END_TEST
 
 static void
 setup_zero()
@@ -70,7 +164,7 @@ setup_zero()
   const uint64_t sz[3] = { 32, 32, 32 };
   const size_t bsize[3] = { 16, 16, 32 };
   zeroes = ookread("/dev/zero", sz, bsize, OOK_U16, 1);
-  ck_assert_int_ne(zeroes, NULL);
+  tjf_ck_ptr_ne(zeroes, NULL);
 }
 
 static void
@@ -136,6 +230,36 @@ START_TEST(zero_rw)
 }
 END_TEST
 
+static void
+setup_simple()
+{
+  FILE* fp = fopen(simplefile, "wb");
+  tjf_ck_ptr_ne(fp, NULL);
+  uint32_t data[16];
+  for(size_t z=0; z < 16; ++z) {
+    for(size_t y=0; y < 16; ++y) {
+      for(size_t x=0; x < 16; ++x) {
+        data[x] = value(x,y,z);
+      }
+      fwrite(data, sizeof(uint32_t), 16, fp);
+    }
+  }
+  fclose(fp);
+  ck_assert(ookinit(StdCIO));
+
+  const uint64_t sz[3] = { 16, 16, 16 };
+  const size_t bsize[3] = { 8, 8, 16 };
+  of = ookread(simplefile, sz, bsize, OOK_U32, 1);
+  tjf_ck_ptr_ne(of, NULL);
+}
+
+static void
+teardown_simple()
+{
+  ck_assert(ookclose(of) == 0);
+  of = NULL; /* force memory being unreachable (for leak checking) */
+}
+
 START_TEST(simple_verify)
 {
   size_t bsize[3];
@@ -175,9 +299,14 @@ rwop_suite()
   tcase_add_test(zero, zero_rw);
   TCase* simple = tcase_create("simple");
   tcase_add_test(simple, simple_verify);
+  TCase* multicomp = tcase_create("multicomp");
+  tcase_add_test(multicomp, multicomp_read);
+
   tcase_add_checked_fixture(zero, setup_zero, teardown_zero);
   tcase_add_checked_fixture(simple, setup_simple, teardown_simple);
+  tcase_add_checked_fixture(multicomp, setup_multicomp, teardown_multicomp);
   suite_add_tcase(s, zero);
   suite_add_tcase(s, simple);
+  suite_add_tcase(s, multicomp);
   return s;
 }
