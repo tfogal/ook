fb991bcbb6d344ca9f2f03b4c9aa00c10336e10f
Tom Fogal <tfogal@alumni.unh.edu>
2013-10-07 21:08:40 -0600

    fix the intra-links for manpages.

--- man/mkman.sh
+++ man/mkman.sh
@@ -11,15 +11,15 @@ for man in $(ls -1 $1/man3/*.3) ; do
   base=$(basename ${man})
   #groff -mandoc -Txhtml ${man} > ${base}.xhtml
   man2html ${m2hopt} ${man} | tail -n +3 | \
-    sed 's,../man3/,/man/,' | \
-    sed 's,../man7/,/man/,' > \
+    sed 's,../man3/,/ook/man/,' | \
+    sed 's,../man7/,/ook/man/,' > \
     ${base}.html
 done
 for man in $(ls -1 $1/man7/*.7) ; do
   base=$(basename ${man})
   man2html ${m2hopt} ${man} | tail -n +3 | \
-    sed s,../man3/,/man/, | \
-    sed s,../man7/,/man/, > \
+    sed s,../man3/,/ook/man/, | \
+    sed s,../man7/,/ook/man/, > \
     ${base}.html
 done
 #
